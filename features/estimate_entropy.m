9f4270a9894c2053822307aaa71534f1e43995f3
sosata <sosata@gmail.com>
2016-04-21 13:54:22 -0500

    update entropy to handle cells

--- features/estimate_entropy.m
+++ features/estimate_entropy.m
@@ -4,10 +4,16 @@ ent = 0;
 
 labs = unique(lab);
 
-for i=labs',
-    p = sum(lab==i)/length(lab);
-    ent = ent - p*log(p);
+if iscell(lab),
+    for i=1:length(labs),
+        p = sum(strcmp(lab,labs{i}))/length(lab);
+        ent = ent - p*log(p);
+    end
+else
+    for i=labs',
+        p = sum(lab==i)/length(lab);
+        ent = ent - p*log(p);
+    end
 end
-
 
 end
--- features/extract_features_communication.m
+++ features/extract_features_communication.m
@@ -1,6 +1,7 @@
 function [feature, feature_label] = extract_features_communication(data)
 
-feature_label = {'call in','call out','call miss','call diff','sms in','sms out','sms diff','resp time'};
+feature_label = {'call in','call out','call miss','call diff','sms in','sms out','sms diff','resp time',...
+    'call miss ratio','call in circ','call out circ','sms in circ','sms out circ','late communication'};
 
 if isempty(data),
     feature = NaN*ones(1,length(feature_label));
@@ -12,18 +13,25 @@ if isempty(data{1}),
     return;
 end
 
+time = data{1};
+
 % Phone features
 phone_in = sum(strcmp(data{4},'PHONE')&strcmp(data{5},'INCOMING'));
 phone_out = sum(strcmp(data{4},'PHONE')&strcmp(data{5},'OUTGOING'));
 phone_miss = sum(strcmp(data{4},'PHONE')&strcmp(data{5},'MISSED'));
 phone_diff = phone_in + phone_miss - phone_out;
+if phone_in~=0,
+    phone_miss_ratio = phone_miss/phone_in;
+else
+    phone_miss_ratio = NaN;
+end
 
 % SMS features
 sms_in = sum(strcmp(data{4},'SMS')&strcmp(data{5},'INCOMING'));
 sms_out = sum(strcmp(data{4},'SMS')&strcmp(data{5},'OUTGOING'));
 sms_diff = sms_in - sms_out;
 
-% Response time
+% SMS Response time
 %TODO: needs to be optimized
 if sms_in==0||sms_out==0,
     t_resp = NaN;
@@ -51,6 +59,19 @@ else
     end
 end
 
-feature = [phone_in,phone_out,phone_miss,phone_diff,sms_in,sms_out,sms_diff,t_resp];
+% circadian rhythms
+circ_call_in = estimate_circadian_rhythmicity(time(strcmp(data{4},'PHONE')&strcmp(data{5},'INCOMING')), 86400);
+circ_call_out = estimate_circadian_rhythmicity(time(strcmp(data{4},'PHONE')&strcmp(data{5},'OUTGOING')), 86400);
+circ_sms_in = estimate_circadian_rhythmicity(time(strcmp(data{4},'SMS')&strcmp(data{5},'INCOMING')), 86400);
+circ_sms_out = estimate_circadian_rhythmicity(time(strcmp(data{4},'SMS')&strcmp(data{5},'OUTGOING')), 86400);
+
+% time
+comm_late = sum((mod(time,86400)>18*3600)|(mod(time,86400)<3*3600))/length(time);
+
+% contacts
+% TODO
+
+feature = [phone_in,phone_out,phone_miss,phone_diff,sms_in,sms_out,sms_diff,t_resp,phone_miss_ratio, ...
+    circ_call_in, circ_call_out, circ_sms_in, circ_sms_out, comm_late];
 
 end
