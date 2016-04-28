a997cdc3619133f5b058f636e1f5c8ad33f370f5
sosata <sosata@gmail.com>
2016-04-28 11:20:06 -0500

    add spearman to mycorr

--- functions/mycorr.m
+++ functions/mycorr.m
@@ -1,8 +1,28 @@
-function r = mycorr(x,y)
+% A version of correlation function that can handle missing (NaN) values
 
-x = x - nanmean(x);
-y = y - nanmean(y);
+function r = mycorr(x,y,type)
 
-r = nanmean(x.*y)/nanstd(x)/nanstd(y);
+if strcmp(type, 'pearson'),
+    
+    x = x - nanmean(x);
+    y = y - nanmean(y);
+
+    r = nanmean(x.*y)/nanstd(x)/nanstd(y);
+
+elseif strcmp(type, 'spearman'),
+    
+    indnanx = find(isnan(x));
+    indnany = find(isnan(y));
+    
+    x(union(indnanx, indnany)) = [];
+    y(union(indnanx, indnany)) = [];
+    
+    r = corr(x,y,'type','spearman');
+    
+else
+    
+    error(['correlation type ',type,' is unknown.']);
+    
+end
 
 end
