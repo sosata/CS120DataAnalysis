c6ab8c15bfc5abec0afe5a2699717ae6fb155fc4
Ted Cybulski <tcybulski@gmail.com>
2016-06-13 09:39:14 -0500

    added print_demo_stats.m
    Simply prints a summary of the demographics survey information for
    participants that completed the study.

---
+++ sleep/print_demo_stats.m
@@ -0,0 +1,32 @@
+clear all
+close all
+
+%% Load Data
+
+d = load('demo_baseline');
+s = load('features_sleepdetection');
+
+subjects = s.subjects_sleep;
+
+%%
+
+subj_idx = ismember(d.subject_baseline, subjects);
+
+% DANGER: size(subj_idx) is 207. Is this the right number?
+
+%% Let's get the demographics as they stand.
+
+names = fieldnames(d);
+fprintf('sleep survey proportions:\n')
+for i = 1:length(names)
+    if ~strcmp(names{i}, 'subject_baseline')
+        field = getfield(d, names{i});
+        total = length(field(subj_idx));
+        fprintf('%s:\n', names{i})
+        uniques = unique(field);
+        for j = 1:length(unique(field))
+            fprintf('\t%s: %f\n', num2str(uniques(j)), ...
+                length(find(field(subj_idx) == j)) / total)
+        end
+    end
+end
